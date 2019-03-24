524db5fbd18eaa58deff5afaf3873459be14138f
tariqyasin <tariq.yasin@outlook.com>
2019-03-24 22:47:32 +0000

    Update my_random_forest.py

--- my_random_forest.py
+++ my_random_forest.py
@@ -1,4 +1,8 @@
-""" An implementation of the random forest regressor. Created with guidance from fastai's Introduction to Machine Learning. See my_random_forest.ipynb for example use, testing and development."""
+""" An implementation of the random forest regressor. 
+Created with guidance from fastai's Introduction to Machine Learning. 
+See my_random_forest.ipynb for example use, testing and development. 
+
+"""
 
 import numpy as np
 import pandas as pd
